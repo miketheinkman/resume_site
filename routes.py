d4a7684dd162ba48896afee2b448daf5b12e289e
Mike <mike_gibbs@rocketmail.com>
2016-06-15 20:24:02 -0500

    The grind Continues

--- routes.py
+++ routes.py
@@ -8,3 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def render_home():
     return render_template('home.html')
+
+
+@app.route('/python')
+def render_python():
+    return render_template('python.html')
+
+
+@app.route('/about')
+def render_about():
+    return render_template('about.html')
